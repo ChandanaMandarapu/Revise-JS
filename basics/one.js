170ec99138a01f7627466a3a10f97b839153f123
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-05-12 12:15:03 +0530

    basics

---
+++ basics/one.js
@@ -0,0 +1,10 @@
+console.log("hello world")
+console.error("error")
+console.warning("warning")
+
+// collecting user input frm node
+
+const prompt = require("prompt-sync")()
+
+const v= prompt("type something ")
+console.log(v)
